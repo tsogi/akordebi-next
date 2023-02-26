6e78b71daff96ff30e1adbd8b1b2c1beb7ede4bd
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2023-02-27 01:55:44 +0400

    Add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
 import '@/styles/globals.css'
+import React from 'react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script 
+      src="https://www.googletagmanager.com/gtag/js?id=G-EQH6P2B20Z">
+      strategy="afterInteractive"
+    </Script>
+    <Script strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-EQH6P2B20Z');
+      `}
+    </Script>
+
+    <Component {...pageProps} />
+  </>
 }
