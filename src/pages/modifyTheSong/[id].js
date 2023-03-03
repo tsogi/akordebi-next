5b2f9bd481bd4ac77f26d08fe6b381dcb0518f10
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2023-03-03 13:38:58 +0400

    Cache

--- src/pages/modifyTheSong/[id].js
+++ src/pages/modifyTheSong/[id].js
@@ -19,7 +19,7 @@ export default function ModifySong({ song }){
     </>
 }
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
     let { id } = params;
     let song = await db.getSong(id);
   
