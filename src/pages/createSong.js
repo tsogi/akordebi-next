f9f015f1112a9027bb03d070b92650a72c30ab72
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2023-03-19 13:42:47 +0400

    Add upload video instructions

--- src/pages/createSong.js
+++ src/pages/createSong.js
@@ -14,7 +14,11 @@ export default function CreateSong(){
         </Head>
         <Header />
         <div className={`${styles.createSongPage} page_container`}>
+            <div className={styles.uploadLabel}>სიმღერის akordebi.ge-ზე გამოსაქვეყნებლად შეავსეთ ველები და ატვირთეთ ტექსტი პლიუს(+) ღილაკების მეშვეობით. პროცესი არის მარტივი და ინტუიტიური. ასევე, ამ გვერდის ბოლოში, შეგიძლიათ ნახოთ <a href="#videoInstruction">ატვირთვის ვიდეო ინსტრუქცია</a></div>
             <SongCreator  />
+            <div id="videoInstruction" className={styles.videoInstruction}>
+                <iframe width="100%" height="500px" src="https://www.youtube.com/embed/HuO8oZXFKgg" title="ატვირთვის ვიდეო ინსტრუქციები" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
         </div>
         <Footer />
     </>
