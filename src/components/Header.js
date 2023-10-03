ac7c024d45f10db539ced4d677126d555b36b75c
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2023-10-03 17:25:29 +0400

    New logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
     return <header className={styles.headerWrapper}>
         <div className={styles.logoArea}>
             <a href="/">
-                <Image alt="akordebiLogo" src={"/akordebi.png"} width={300} height={124} />
+                <Image alt="akordebiLogo" src={"/akordebige_logo.svg"} width={300} height={300} />
             </a>
         </div>
         <div></div>
