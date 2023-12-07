8d5d51778679bde7ab778248676850cdb971c174
Mohamed Abdelmaksoud <medohmada1623@gmail.com>
2023-12-07 15:55:57 +0200

    added Enter button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import styles from "./Header.module.css";
 import Image from "next/image";
 
 export default function Header(){
-    return <header className={styles.headerWrapper}>
+    return <header className={`page_container ${styles.headerWrapper}`}>
         <div className={styles.logoArea}>
             <a href="/">
                 <img className={styles.logoImage} alt="akordebiLogo" src={"/akordebige_logo.svg"} />
             </a>
         </div>
-        <div></div>
+        <div className={styles.authButton}>
+            <Link href="/auth/login">შესვლა</Link>
+        </div>
     </header>
 }
