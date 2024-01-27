b11e7ac6b280ed77564764d52c2f5334ff2e41ff
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2024-01-27 14:23:19 +0400

    Remove snowfall

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,6 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 import { MyUserContextProvider } from '@/utils/useUser';
 import AuthSlideOver from '@/components/AuthSlideOver';
-import Snowfall from 'react-snowfall';
 import GoogleTagManager from '@/components/GoogleAnalytics';
 
 export default function App({ Component, pageProps }) {
@@ -18,8 +17,6 @@ export default function App({ Component, pageProps }) {
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
-      <Snowfall snowflakeCount={50} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }} />
-
       <MyUserContextProvider>
         <Component {...pageProps} />
         <AuthSlideOver />
