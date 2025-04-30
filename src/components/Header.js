4d7133492ace760abaa702149e39d7634eff40eb
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2025-04-30 11:04:17 +0400

    Next step

--- src/components/Header.js
+++ src/components/Header.js
@@ -125,7 +125,7 @@ export default function Header(){
                     </div>
 
                     <div className={styles.headerActions}>
-                        {languageSwitcher()}
+                        {/* {languageSwitcher()} */}
                         <button 
                             className={styles.menuButton}
                             onClick={() => setIsMenuOpen(!isMenuOpen)}
