2bada1a126c45a07d57ee492c205514268f11fb6
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2025-05-29 00:55:14 +0400

    Next step

--- src/utils/notations.js
+++ src/utils/notations.js
@@ -1,8 +1,26 @@
-export const notations = [
-    { code: "chords", name: "გიტარის აკორდები", chordsDir: "/chords/guitar"},
-    { code: "tabs", name: "გიტარის ტაბები", chordsDir: "/chords/guitar" },
-    { code: "fanduri", name: "ფანდურის აკორდები", chordsDir: "/chords/fanduri" },
+export const notations_old = [
+    { code: "chords", category: "guitar", name: "გიტარის აკორდები", chordsDir: "/chords/guitar"},
+    { code: "tabs", category: "guitar", name: "გიტარის ტაბები", chordsDir: "/chords/guitar" },
+    { code: "fanduri", category: "fanduri", name: "ფანდურის აკორდები", chordsDir: "/chords/fanduri" },
 ];
+
+export const notations = [
+    { 
+        name: "გიტარა", 
+        icon: "/icons/guitar.svg",
+        tabs: [
+            { code: "chords", category: "guitar", name: "გიტარის აკორდები", chordsDir: "/chords/guitar"},
+            { code: "tabs", category: "guitar", name: "გიტარის ტაბები", chordsDir: "/chords/guitar" },
+        ] 
+    },
+    {
+        name: "ფანდური", 
+        icon: "/icons/fanduri.svg",
+        tabs: [
+            { code: "fanduri", category: "fanduri", name: "ფანდურის აკორდები", chordsDir: "/chords/fanduri" },
+        ] 
+    }
+]
 
 export function getNotation(code){
     const notation = notations.find(notation => notation.code === code);
