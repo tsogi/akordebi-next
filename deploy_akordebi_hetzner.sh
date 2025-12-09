a52d09e4c92eb46c23280de7ed5fb9648fdf2969
Nika Tsogiaidze <Tsogiaidze@yahoo.com>
2025-12-10 02:12:27 +0400

    Hetzner

--- deploy_akordebi_hetzner.sh
+++ deploy_akordebi_hetzner.sh
@@ -10,30 +10,21 @@ npm run build
 # Create docker image .tar for intel
 docker buildx build --platform linux/amd64 -t akordebi:1.4 --output "type=docker,dest=./akordebi.tar" .
 
-# Upload docker image .tar to ssh
-scp -i /Users/nika/Projects/akordebi.ge/akordebi_ec2.pem ./akordebi.tar ubuntu@18.153.218.239:/var/projects
-# scp -i /users/nika/.ssh/id_rsa_ubuntu_server ./akordebi.tar nika@tsogi.net:/var/projects
+# Upload docker image .tar to Hetzner
+scp -i /Users/nika/.ssh/id_ed25519hetzer ./akordebi.tar root@46.224.126.7:/var/projects
 
 # Connect to the machine and run docker image
-ssh -i /Users/nika/Projects/akordebi.ge/akordebi_ec2.pem ubuntu@18.153.218.239 << EOF
+ssh -i /Users/nika/.ssh/id_ed25519hetzer root@46.224.126.7 << EOF
 
     # Navigating to the directory
-    cd /var/projects
+    cd /var/projects/backup_ec2_server
     
     # Executing the commands
     docker-compose stop akordebi
     docker-compose rm -f akordebi
-    docker load -i akordebi.tar
+    docker load -i /var/projects/akordebi.tar
     docker-compose up -d akordebi
     docker system prune -f
-
-
-    # docker-compose rm -f akordebi
-    # docker load -i akordebi.tar
-    # docker-compose up -d akordebi
-
-    # Delete all unused containers, networks, images
-    # docker system prune -f
 
 EOF
 
